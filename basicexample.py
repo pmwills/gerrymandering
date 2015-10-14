d1de70e6543abe5cb04cca3ce422f169da9450da
Peter Wills <peter.m.wills@gmail.com>
2015-10-14 17:43:26 -0400

    Fix touching

--- basicexample.py
+++ basicexample.py
@@ -2,26 +2,21 @@ __author__ = 'sarahbaugh'
 from Node import Node
 from script import connectToDatabase
 
-c,db = connectToDatabase()
+c, db = connectToDatabase()
 c2, db = connectToDatabase()
 c.execute("SELECT ALL OID_, USH_RVOTE_, USH_DVOTE_ FROM minnesota")
 
 nodes = []
 for (OID_, USH_RVOTE_, USH_DVOTE_) in c:
-
-    #print "repub"  + str(x), "dem" + str(y), "z" + str(z)
+    # print "repub"  + str(x), "dem" + str(y), "z" + str(z)
 
     #print "VOTES" + str(nodes[j].votes)
     c2.execute("SELECT p2 FROM minnesotaTouching WHERE p1 = " + str(OID_))
     touching = [x for x in c2]
-    touching = []
-    for x in c2:
-        touching.append(x)
     id = OID_
     votes = int(USH_RVOTE_) + int(USH_DVOTE_)
     newNode = Node(id, touching, votes)
     nodes.append(newNode)
-
 
 
 def main():
